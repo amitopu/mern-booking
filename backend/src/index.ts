7445be60d5c71bdeec78903466ecedd66ddd8cfb
amitopu <nurealamsiddiky@gmail.com>
2024-02-04 09:24:23 +0600

    cloudinary is configured with the project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,13 @@ import mongoose from "mongoose";
 import userRouter from "./routes/User";
 import authRouter from "./routes/Auth";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
     console.log(
